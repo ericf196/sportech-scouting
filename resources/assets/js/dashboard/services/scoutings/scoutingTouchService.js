61e02294284e8ded6ce57ddbc5df9715f1691cb3
Pedro Gorrin <pedro.gorrin@ngncloud.com>
2017-01-31 08:54:24 -0400

    no message

--- resources/assets/js/dashboard/services/scoutings/scoutingTouchService.js
+++ resources/assets/js/dashboard/services/scoutings/scoutingTouchService.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export default {
     update(id, scouting, successCalback, errorCallback){
-        return Vue.http.put('/api/scoutings/' + id + '/touches/', scouting, {emulateHTTP: true})
+        return Vue.http.put('/api/scoutings/' + id + '/touches/', scouting)
             .then((response) => successCalback(response.body))
             .catch((error) => errorCallback(error.body));
     },
