a4fb0a1a2642637147dc9ec14d5c5c4b6d625407
Yurianny <yurianny.canizalez@sportech37.com>
2017-06-01 17:08:14 -0400

    Credentials

---
+++ resources/views/emails/credential.blade.php
@@ -0,0 +1,12 @@
+<p>Hola,</p>
+
+<p>Te han invitado a Registrarte en Scouting37.</p>
+<div class="">
+    <img width="300px" src="assets/img/logo.png" alt="">
+    <h1>Data analytics for fencers</h1>
+    <h1>Data analytics for fencers</h1>
+
+    <span class="">Log in</span>
+</div>
+
+<a href="{{ url('http://localhost:4200/register?token='.$invite->token) }}">Click</a>  para registrarse!!
